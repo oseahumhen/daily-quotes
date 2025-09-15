2f7e922f443f8f96000a3f30f9ca1621cb817085
oseahumhen <okokhereoseahumhen@gmail.com>
2025-09-15 21:41:51 +0100

    initial spring init files

---
+++ src/main/java/com/example/dailyquotes/DailyQuotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.dailyquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyQuotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dailyquotes/DailyQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailyquotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
